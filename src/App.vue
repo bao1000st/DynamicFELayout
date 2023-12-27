fbaba25afcbd673c4569f3fe48f2d1bbfc02e50f
nmhoang1000 <nmhoang1000@gmail.com>
2023-12-27 22:02:58 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>hello</h1>
+</template>
+
+<script>
+new Vue({
+  el: 'body',
+  data: {
+    currentComponent: null,
+    componentsArray: ['foo', 'bar']
+  },
+  components: {
+    'foo': {
+      template: '<h1>Foo component</h1>'
+    },
+    'bar': {
+      template: '<h1>Bar component</h1>'
+    }
+  },
+  methods: {
+    swapComponent: function (component) {
+      this.currentComponent = component;
+    }
+  }
+})
+</script>
